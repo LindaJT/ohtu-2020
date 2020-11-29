69042ad912021056100f267325daa679b35b8e57
LindaJT <jokinenlindak@gmail.com>
2020-11-29 10:23:07 +0200

    tehtävä 2

---
+++ viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -0,0 +1,23 @@
+package statistics;
+
+import statistics.matcher.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String url = "https://nhlstatisticsforohtu.herokuapp.com/players.txt";
+
+        Statistics stats = new Statistics(new PlayerReaderImpl(url));
+          
+        Matcher m = new And( 
+            new HasFewerThan(1, "goals"), 
+            new PlaysIn("NYR")
+        );
+       
+        
+        for (Player player : stats.matches(m)) {
+            System.out.println(player);
+        }
+        
+        System.out.println(stats.matches(new All()).size());
+    }
+}
---
+++ viikko6/QueryLanguage/src/main/java/statistics/matcher/All.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package statistics.matcher;
+
+import statistics.Player;
+
+/**
+ *
+ * @author linjokin
+ */
+public class All implements Matcher {
+
+    public boolean matches(Player arg0) {
+        return true;
+    }
+    
+}
